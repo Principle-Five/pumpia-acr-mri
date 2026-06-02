1028d6382bcd1fbd4dfc8a658d4dd088e5d94ee1
Zack <150851679+zravetz@users.noreply.github.com>
2026-06-02 20:17:34 +0100

    Changes to be committed: 	new file:   dicoms.nuitka-package.config.yml 	modified:   pumpia_acr_mri/scripts/run_large_acr_rpt.py 	modified:   pumpia_acr_mri/scripts/run_large_acr_rpt_old.py 	modified:   pumpia_acr_mri/scripts/run_med_acr_rpt.py

--- pumpia_acr_mri/scripts/run_large_acr_rpt_old.py
+++ pumpia_acr_mri/scripts/run_large_acr_rpt_old.py
@@ -1,3 +1,14 @@
+# command line "python -m nuitka .\pumpia_acr_mri\scripts\run_large_acr_rpt_old.py"
+
+# tk-inter is needed for tkinter to work with nuitka
+# nuitka documentation suggests it is not explicitly needed/found automatically but this is wrong
+# (possibly due to inclusion of matplotlib)
+
+# nuitka-project: --enable-plugin=tk-inter
+# nuitka-project: --mode=onefile
+# nuitka-project: --windows-console-mode=disable
+# nuitka-project: --user-package-configuration-file=dicoms.nuitka-package.config.yml
+
 from pumpia_acr_mri.large_old.acr_mri_rpt_collection import LargeACRrptCollection
 
 
--- pumpia_acr_mri/scripts/run_med_acr_rpt.py
+++ pumpia_acr_mri/scripts/run_med_acr_rpt.py
@@ -1,3 +1,14 @@
+# command line "python -m nuitka .\pumpia_acr_mri\scripts\run_med_acr_rpt.py"
+
+# tk-inter is needed for tkinter to work with nuitka
+# nuitka documentation suggests it is not explicitly needed/found automatically but this is wrong
+# (possibly due to inclusion of matplotlib)
+
+# nuitka-project: --enable-plugin=tk-inter
+# nuitka-project: --mode=onefile
+# nuitka-project: --windows-console-mode=disable
+# nuitka-project: --user-package-configuration-file=dicoms.nuitka-package.config.yml
+
 from pumpia_acr_mri.medium.acr_mri_rpt_collection import MedACRrptCollection
 
 
